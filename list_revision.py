e1114cbfd520db5335c03ae2f3980766ae122a06
Taye Abidakun <tayeabidakun@gmail.com>
2025-09-13 14:57:28 +0100

    list_revision

---
+++ list_revision.py
@@ -0,0 +1,20 @@
+vals = [9, 30, 50, 70, 4]
+result = []
+
+for num in vals:
+    multiply = num * 2
+    result.append(multiply)
+
+print(result)  
+# print(f"The name of my school is {schools[0]}")
+# print(f"The name of my school is {schools[1]}")
+# print(f"The name of my school is {schools[2]}")
+
+# schools.append("OAU")
+# schools.append("MIT")
+# print(schools)
+# print(abc)
+# print(False)
+
+
+
