5a5ed75af046e32659b4fb70e9969a824d47466d
Taye Abidakun <tayeabidakun@gmail.com>
2025-09-29 17:39:52 +0100

    list of dictionary in Python

---
+++ list_of_dictionary.py
@@ -0,0 +1,15 @@
+
+users = [
+            {"firstname": "Taye", "lastname": "Abidakun"},
+            {"firstname": "Patience", "lastname": "Pat"}
+
+        ]
+
+# 1. To add a new user
+
+# 2. To display lastname of all users
+
+# 1.
+#     Firstname
+#     Lastname
+# 2. Bye
