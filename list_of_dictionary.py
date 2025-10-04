216768d96aa85e479310a7d7373c6750fdff8852
Taye Abidakun <tayeabidakun@gmail.com>
2025-10-04 08:29:13 +0100

    list of dictionary assignment

--- list_of_dictionary.py
+++ list_of_dictionary.py
@@ -5,11 +5,26 @@ users = [
 
         ]
 
-# 1. To add a new user
 
-# 2. To display lastname of all users
 
-# 1.
-#     Firstname
-#     Lastname
-# 2. Bye
+def user_func():
+    main_menu = input("1. Add User 2. Display lastname of all users: ")
+    if(main_menu== "1"):
+        newuser_firstname = input("Enter the firstname: ")
+        newuser_lastname = input("Enter the lastname: ")
+        newuser = {"firstname": newuser_firstname, "lastname": newuser_lastname}
+        users.append(newuser)
+        print(users)
+    elif(main_menu == "2"):
+        for user in users:
+            print(user["lastname"])
+    go_back = input("Do you want to do any other thing? ")
+    if(go_back == "1"):
+        user_func()
+    else:
+        print("BYE!!!")
+
+
+user_func()
+
+
