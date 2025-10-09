f850c33f36fb45b2277d6dda9d0d5bce54dc686d
Taye Abidakun <tayeabidakun@gmail.com>
2025-10-09 13:49:40 +0100

    set and tuple

---
+++ param.py
@@ -0,0 +1,22 @@
+# def show_taye():
+#     name = "Taye"
+#     print(f"My name is {name}")
+
+# def show_kenny():
+#     name = "Kenny"
+#     print(f"My name is {name}")
+
+# def show_patience():
+#     name = "Patience"
+#     print(f"My name is {name}")
+
+# show_taye()
+# show_kenny()
+# show_patience()
+
+def show_person(name, school):
+    print(f"My name is {name}, I study at {school}")
+
+show_person("Taye", "UI")
+show_person("Kenny", "OAU")
+show_person("Patience", "Lead")
---
+++ tuple.py
@@ -0,0 +1,13 @@
+# names = ("Peter", "Patience", "Peace", "Peter")
+# new_names = list(names)
+# new_names.append("John")
+# names = tuple(new_names)
+# print(names)
+
+# ctrl / for commenting
+# names = {"John", "James", "Jonah", "John", "John"}
+
+# print(names)
+
+
+
